ba90df4c616d92b696cb074a99d23ff2a4f99a54
Marcell Huszti <rashan86+github@gmail.com>
2014-03-04 16:11:33 +0100

    működik a több sor mentése

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,15 +1,13 @@
 var controllers = angular.module('trackIt',[]);
 
 controllers.controller('saveTrack', ['$scope', function ($scope) {
-	$scope.list = {};
+	
+	$scope.list = [];
+
 	$scope.add = function (trackTime) {
 		
 		$scope.addedLine = angular.copy($scope.trackTime);	
-
-		var current = $scope.list;
-		var newList = current.append($scope.trackTime);
-		$scope.list = newList;
-
+		$scope.list.push($scope.trackTime);
 }
 
 }]);
