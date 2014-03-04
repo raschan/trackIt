56f1c8471da3a05172121cb0f9aba4c7914c1fc5
Marcell Huszti <rashan86+github@gmail.com>
2014-03-04 16:00:31 +0100

    sor hozzáfűzés

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,7 +1,15 @@
 var controllers = angular.module('trackIt',[]);
 
 controllers.controller('saveTrack', ['$scope', function ($scope) {
+	$scope.list = {};
 	$scope.add = function (trackTime) {
-		$scope.addedLine = angular.copy($scope.trackTime);		
-	}
+		
+		$scope.addedLine = angular.copy($scope.trackTime);	
+
+		var current = $scope.list;
+		var newList = current.append($scope.trackTime);
+		$scope.list = newList;
+
+}
+
 }]);
