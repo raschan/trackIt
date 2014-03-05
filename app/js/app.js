9f3466007bdf0ae7fb53737098d16fc404d4ce6b
Marcell Huszti <rashan86+github@gmail.com>
2014-03-05 16:35:41 +0100

    van user kezelés, de még nem működik

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,29 @@
-var trackIt = angular.module('trackIt', []);
+var trackIt = angular.module('trackIt', 
+	[
+	'trackIt.services', 
+	'trackIt.controllers',
+	'trackIt.directives',
+	'ngRoute',
+	'UserApp'
+	]);
+
+trackIt.run(function($rootScope, user){
+	user.init({ appId: 'trackIt'});	
+});
+
+
+//routing
+trackIt.config(['$routeProvider',function($routeProvider) {
+	$routeProvider.
+	when('/login', 
+		{
+			templateUrl: 'partials/login.html',
+			login: true
+		}).
+	when('/signup', 
+		{
+			templateUrl: 'partials/signup.html',
+			public: true
+		}).
+	otherwise({redirectTo: '/home'});
+}]);
